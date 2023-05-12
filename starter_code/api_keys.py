edbbc37d7a5ff785bfbb2a6b7bffe07d8ccedd65
Yi-Hsiang Chou <yi-hsiangchou@Yi-Hsiangs-MacBook-Pro.local>
2023-05-11 22:09:20 -0500

    adding homework

---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "47c502d52168c544380d89c484667fae"
+
+# Geoapify API Key
+geoapify_key = "e8c8a92cf3b6413d9544611d5d5132a2"
---
+++ starter_code/api_keys_copy.py
@@ -0,0 +1,2 @@
+# Geoapify API Key
+geoapify_key = "e8c8a92cf3b6413d9544611d5d5132a2"
